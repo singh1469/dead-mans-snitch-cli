55d3664f36cb8ad8bf729f1504429746b672cabf
hardiarl <sngh1469@gmail.com>
2016-02-14 21:58:51 +0000

    bugfixes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ##This is work in progress - pet project to get familiar with es6 harmony
 
 #####make user of let syntax
-#####implement way of allowing user to specify key
+#####comments
+#####check code
--- index.js
+++ index.js
@@ -3,22 +3,31 @@
 //main file, entry point
 
 var program = require('commander');
+var auth = require('./lib/auth.js');
 var snitch = require('./lib/snitch.js');
 var constant = require('./lib/constant.js');
 var response = require('./lib/response.js');
 var chalk = require('chalk');
+
+var api_key = auth.getKey();
+
 
 program
   .version('1.0.0')
   .option('-l, --list', 'List all snitches')
   .option('-h, --healthy', 'List healthy snitches')
   .option('-u, --unhealthy', 'List unhealthy snitches')
-  .option('k, --key', 'API key')
   .parse(process.argv);
+
+if (api_key === '') {
+  var message = chalk.red.bold('Sorry, no api key found. Try setting DEAD_MANS_SNITCH_KEY as an environment variable containing your api key');
+  response.write(message);
+  return;
+}
 
 if (program.list) {
   //get list of all snitches
-  snitch.list().then(function(items) {
+  snitch.list(api_key).then(function(items) {
     //generate client response
     var output = response.generate(items);
     if (output === false) {
@@ -31,11 +40,12 @@ if (program.list) {
       var output = 'Sorry, there was an error, check the logs';
       response.write(output);
     });
+  return;
 }
 
 if (program.healthy) {
   //get list of healthy snitches
-  snitch.list().then(function(items) {
+  snitch.list(api_key).then(function(items) {
     //specify type of snitches to include - healthy
     var output = response.generate(items, constant.STATUS_HEALTHY);
     if (output === false) {
@@ -48,11 +58,12 @@ if (program.healthy) {
       var output = 'Sorry, there was an error, check the logs';
       response.write(output);
     });
+  return;
 }
 
 if (program.unhealthy) {
   //get list of unhealthy snitches
-  snitch.list().then(function(items) {
+  snitch.list(api_key).then(function(items) {
     //specify type of snitches to include - unhealthy
     var output = response.generate(items, constant.STATUS_UNHEALTHY);
     //generate client response
@@ -66,4 +77,5 @@ if (program.unhealthy) {
       var output = 'Sorry, there was an error, check the logs';
       response.write(output);
     });
+  return;
 }
